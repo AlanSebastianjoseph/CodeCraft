1fccec4a054ff09f8c9685bbf52c17e5c36ad5f3
AlanSebastianjoseph <alansebastianjoseph2029@mca.ajce.in>
2025-09-30 10:05:48 +0530

    Update insertionlinkedlist.c

--- insertionlinkedlist.c
+++ insertionlinkedlist.c
@@ -1,63 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
-}
-
-void insertEnd(struct Node** head, int value) {
-    struct Node* newNode = createNode(value);
-    if (*head == NULL) {
-        *head = newNode;
-        return;
-    }
-    struct Node* temp = *head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-}
-
-
-void displayList(struct Node* head) {
-    printf("Roll Numbers in Linked List: ");
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d", temp->data);
-        if (temp->next != NULL) {
-            printf(" -> ");
-        }
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-int main() {
-    struct Node* head = NULL;
-
-
-    insertEnd(&head, 42);
-
-
-    insertEnd(&head, 14);
-    insertEnd(&head, 13);
-    insertEnd(&head, 30);
-
-
-    displayList(head);
-
-    return 0;
-}
-
-
-
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+struct Node { 
+    int data; 
+    struct Node* next; 
+}; 
+ 
+struct Node* createNode(int value) { 
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node)); 
+    newNode->data = value; 
+    newNode->next = NULL; 
+    return newNode; 
+} 
+ 
+void insertEnd(struct Node** head, int value) { 
+    struct Node* newNode = createNode(value); 
+    if (*head == NULL) { 
+        *head = newNode; 
+        return; 
+    } 
+    struct Node* temp = *head; 
+    while (temp->next != NULL) { 
+        temp = temp->next; 
+    } 
+    temp->next = newNode; 
+} 
+ 
+//   Function to add roll number 7 
+void addRoll7(struct Node** head) { 
+    insertEnd(head, 7); 
+} 
+ 
+void displayList(struct Node* head) { 
+    printf("Roll Numbers in Linked List: "); 
+    struct Node* temp = head; 
+    while (temp != NULL) { 
+        printf("%d", temp->data); 
+        if (temp->next != NULL) { 
+            printf(" -> "); 
+        } 
+        temp = temp->next; 
+    } 
+    printf("\n"); 
+} 
+ 
+int main() { 
+    struct Node* head = NULL; 
+ 
+    insertEnd(&head, 42); 
+    insertEnd(&head, 14); 
+    insertEnd(&head, 13); 
+//    
+Now add roll number 7 using special function 
+addRoll7(&head); 
+displayList(head); 
+return 0; 
+} 
